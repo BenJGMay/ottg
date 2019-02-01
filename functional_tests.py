0d3d48b1b8e506b6a000c2b95bdafa7fc870fa22
May <ben.may@mullenloweprofero.com>
2019-02-01 14:15:49 +0000

    Brought comments in with style

--- functional_tests.py
+++ functional_tests.py
@@ -27,13 +27,14 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list.
 
         # There is still a text box inviting her to add another item.
-        # She enters "use peacock feathers to make a fly" (Edith is very methodical)
+        # She enters "use peacock feathers to make a fly"
+        # (Edith is very methodical)
 
         # The page updates again and now shows both items on her list
 
-        # Edith wonders whether the site will remember her list. Then she sees that
-        # the site has generated a unique url for her -- there is some explanatory
-        # text to that effect.
+        # Edith wonders whether the site will remember her list. Then she
+        # sees that the site has generated a unique url for her -- there is
+        # some explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
 
